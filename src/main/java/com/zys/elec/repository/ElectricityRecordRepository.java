8b01db233c4f73d60f27d0dac35a7376888c609d
Jing Ye <2419280542@qq.com>
2024-12-31 19:05:04 +0800

    Temp

--- src/main/java/com/zys/elec/repository/ElectricityRecordRepository.java
+++ src/main/java/com/zys/elec/repository/ElectricityRecordRepository.java
@@ -1,6 +1,7 @@
 package com.zys.elec.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.zys.elec.entity.ElectricityRecord;
@@ -13,14 +14,19 @@ import java.util.Optional;
 @Repository
 public interface ElectricityRecordRepository extends JpaRepository<ElectricityRecord, Long> {
 
-    Optional<ElectricityRecord> findByUser(User user);
+    Optional<List<ElectricityRecord>> findByUser(User user);
 
     Optional<List<ElectricityRecord>> findByRecordDate(LocalDate date);
 
     Optional<List<ElectricityRecord>> findByRecordDateBetween(LocalDate startDate, LocalDate endDate);
 
-    Optional<List<ElectricityRecord>> findByRecordDateBetweenAndUser(LocalDate startDate, LocalDate endDate, User userId);
+    Optional<List<ElectricityRecord>> findByRecordDateBetweenAndUser(LocalDate startDate, LocalDate endDate,
+            User userId);
 
     Optional<List<ElectricityRecord>> findByUserAndRecordDate(User user, LocalDate recordDate);
+
+
+    @Query("SELECT er FROM ElectricityRecord er WHERE er.id NOT IN (SELECT p.electricityRecord.id FROM Predict p)")
+    Optional<List<ElectricityRecord>> findRecordsWithoutPredicts();
 
 }
--- src/main/java/com/zys/elec/service/impl/LoginServiceImpl.java
+++ src/main/java/com/zys/elec/service/impl/LoginServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import com.zys.elec.common.ServiceResult;
 import com.zys.elec.dto.LoginDTO;
 import com.zys.elec.dto.UserDTO;
+import com.zys.elec.service.JWTAuthService;
 import com.zys.elec.service.LoginService;
 import com.zys.elec.service.UserService;
 
@@ -15,11 +16,23 @@ public class LoginServiceImpl implements LoginService {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private JWTAuthService jwtService;
+
     @Override
     public ServiceResult<UserDTO> login(LoginDTO user) {
         // var un = user.getUsername();
         // var pw = user.getPassword();
         var res = userService.checkPassword(user.getUsername(), user.getPassword());
+
+        var un = res.getData().getUsername();
+        var uid = res.getData().getId();
+        var captcha = user.getCaptcha();
+        
+        var jwt = jwtService.generateToken(un,uid.toString(),captcha);
+
+        res.getData().setJwt(jwt);
+
         if (res.isSuccess()) {
             return ServiceResult.success(res.getData());
         } else {

35b069f65a5075fef30ba9ee63ff87f331d1e24d
Jing Ye <2419280542@qq.com>
2025-01-06 00:06:46 +0800

    add plan around

--- src/main/java/com/zys/elec/controller/PlanController.java
+++ src/main/java/com/zys/elec/controller/PlanController.java
@@ -3,8 +3,75 @@ package com.zys.elec.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zys.elec.common.ResponseResult;
+import com.zys.elec.entity.Plan;
+import com.zys.elec.service.PlanService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @RestController
 @RequestMapping("/plan")
 public class PlanController {
-    
+    @Autowired
+    private PlanService planService;
+
+    @GetMapping("/getuserplans")
+    public ResponseResult<List<Plan>> getMethodName(@RequestParam Long userId) {
+        var res = planService.listPlans(userId);
+
+        if (res.isSuccess()) {
+            return ResponseResult.success(res.getData());
+        } else {
+            return ResponseResult.failure(res.getMessage());
+        }
+    }
+
+    @GetMapping("/create")
+    public ResponseResult<Void> createPlan(@RequestParam Plan plan) {
+        var res = planService.createPlan(plan);
+
+        if (res.isSuccess()) {
+            return ResponseResult.success(null);
+        } else {
+            return ResponseResult.failure(res.getMessage());
+        }
+    }
+
+    @GetMapping("/update")
+    public ResponseResult<Void> updatePlanContent(Plan plan) {
+        var res = planService.updatePlanContent(plan);
+
+        if (res.isSuccess()) {
+            return ResponseResult.success(null);
+        } else {
+            return ResponseResult.failure(res.getMessage());
+        }
+    }
+
+    @GetMapping("/complete")
+    public ResponseResult<Void> completePlan(@RequestParam Long planId) {
+        var res = planService.completePlan(planId);
+
+        if (res.isSuccess()) {
+            return ResponseResult.success(null);
+        } else {
+            return ResponseResult.failure(res.getMessage());
+        }
+    }
+
+    @GetMapping("/cancel")
+    public ResponseResult<Void> cancelPlan(@RequestParam Long planId, @RequestParam Long userId) {
+        var res = planService.cancelPlan(planId, userId);
+
+        if (res.isSuccess()) {
+            return ResponseResult.success(null);
+        } else {
+            return ResponseResult.failure(res.getMessage());
+        }
+    }
+
 }
